e4c27b35f41564f35cbfdb338df3c94d7f92ddce
Eanew <H.Eanew@yandex.ru>
2020-03-08 00:45:24 +0300

    Сделал первое задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
-var photosCount = 25;
-var PHOTO_URL_TEMPLATE = 'photos/{{i}}.jpg';
-
-var commentsCount = 6;
-var messages = [
+var MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -12,11 +8,12 @@ var messages = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-var names = ['Морриган', 'Алистер', 'Лелиана', 'Стэн', 'Шейла', 'Зевран'];
-var AVATAR_URL_TEMPLATE = 'img/avatar-{{случайное число от 1 до 6}}.svg';
+var NAMES = ['Морриган', 'Алистер', 'Лелиана', 'Стэн', 'Шейла', 'Зевран'];
 var avatars = [];
+var AVATAR_URL_TEMPLATE = 'img/avatar-{{случайное число от 1 до 6}}.svg';
+var COMMENTS_COUNT = 6;
 
-for (var i = 0; i < commentsCount; i++) {
+for (var i = 0; i < COMMENTS_COUNT; i++) {
   avatars[i] = AVATAR_URL_TEMPLATE.replace('{{случайное число от 1 до 6}}', (i + 1));
 }
 
@@ -31,11 +28,11 @@ var getUniqueRandomItem = function (collection) {
 
 var makeComments = function () {
   var avatarsCloned = avatars.slice();
-  var messagesCloned = messages.slice();
-  var namesCloned = names.slice();
+  var messagesCloned = MESSAGES.slice();
+  var namesCloned = NAMES.slice();
   var comments = [];
 
-  for (var j = 0; j < commentsCount; j++) {
+  for (var j = 0; j < COMMENTS_COUNT; j++) {
     comments[j] = {
       avatar: getUniqueRandomItem(avatarsCloned),
       message: getUniqueRandomItem(messagesCloned),
@@ -46,22 +43,48 @@ var makeComments = function () {
   return comments;
 };
 
-var makePhotos = function () {
+var PHOTO_URL_TEMPLATE = 'photos/{{i}}.jpg';
+var photosCount = 25;
+
+var makePhotoTemplates = function () {
+  var comments = makeComments();
+  var commentsAmount = getRandomCount(1, COMMENTS_COUNT);
   var photos = [];
-  var comments;
-  var commentsAmount;
 
   for (i = 0; i < photosCount; i++) {
-    comments = makeComments();
-    commentsAmount = getRandomCount(1, commentsCount);
-
     photos[i] = {
       url: PHOTO_URL_TEMPLATE.replace('{{i}}', (i + 1)),
       description: 'Описание фотографии', // Не реализовано
       likes: getRandomCount(15, 200),
       comments: comments.slice(0, commentsAmount)
     };
+    comments = makeComments(); // Обновляет случайные комментарии
+    commentsAmount = getRandomCount(1, COMMENTS_COUNT); // Обновляет случайное количество случайных комментариев
   }
 
   return photos;
 };
+
+var pictures = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+var renderPicture = function (photoTemplate) {
+  var picture = pictureTemplate.cloneNode(true);
+  picture.querySelector('.picture__img').src = photoTemplate.url;
+  picture.querySelector('.picture__likes').textContent = photoTemplate.likes;
+  picture.querySelector('.picture__comments').textContent = photoTemplate.comments.length;
+
+  return picture;
+};
+
+var makePictures = function () {
+  var fragment = document.createDocumentFragment();
+  var photos = makePhotoTemplates();
+  for (i = 0; i < photos.length; i++) {
+    fragment.appendChild(renderPicture(photos[i]));
+  }
+
+  return fragment;
+};
+
+pictures.appendChild(makePictures());
