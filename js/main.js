a33e724c5c2755f8e1853dd844db7f2353851cd5
Eanew <H.Eanew@yandex.ru>
2020-03-08 18:19:05 +0300

    Заполненил блок big-picture информацией js-массива

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ var photosCount = 25;
 
 var makePhotoTemplates = function () {
   var comments = makeComments();
-  var commentsAmount = getRandomCount(1, COMMENTS_COUNT);
+  var commentsAmount = getRandomCount(2, COMMENTS_COUNT);
   var photos = [];
 
   for (i = 0; i < photosCount; i++) {
@@ -61,7 +61,7 @@ var makePhotoTemplates = function () {
       comments: comments.slice(0, commentsAmount)
     };
     comments = makeComments(); // Обновляет случайные комментарии
-    commentsAmount = getRandomCount(1, COMMENTS_COUNT); // Обновляет случайное количество случайных комментариев
+    commentsAmount = getRandomCount(2, COMMENTS_COUNT); // Обновляет случайное количество случайных комментариев
   }
 
   return photos;
@@ -79,9 +79,10 @@ var renderPicture = function (photoTemplate) {
   return picture;
 };
 
+var photos = makePhotoTemplates();
+
 var makePictures = function () {
   var fragment = document.createDocumentFragment();
-  var photos = makePhotoTemplates();
   for (i = 0; i < photos.length; i++) {
     fragment.appendChild(renderPicture(photos[i]));
   }
@@ -90,3 +91,27 @@ var makePictures = function () {
 };
 
 pictures.appendChild(makePictures());
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+bigPicture.querySelector('.big-picture__img').querySelector('img').src = photos[0].url;
+bigPicture.querySelector('.likes-count').textContent = photos[0].likes;
+bigPicture.querySelector('.comments-count').textContent = photos[0].comments.length;
+bigPicture.querySelector('.social__caption').textContent = photos[0].description;
+
+var commentsList = bigPicture.querySelector('.social__comments');
+var comments = commentsList.querySelectorAll('.social__comment');
+
+for (i = 0; i < comments.length; i++) {
+  comments[i].querySelector('.social__picture').src = photos[0].comments[i].avatar;
+  comments[i].querySelector('.social__picture').alt = photos[0].comments[i].name;
+  comments[i].querySelector('.social__text').textContent = photos[0].comments[i].message;
+}
+
+
+// var commentPicture = comments.querySelector('.social__picture');
+// var commentText = comments.querySelector('.social__text');
+// commentPicture.src = '{{аватар}}';
+// commentPicture.alt = '{{имя комментатора}}';
+// commentText.textContent = '{{текст комментария}}';
+
