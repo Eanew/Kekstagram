8723a17abf74e60476e98d20101de14c7fb1f976
Eanew <H.Eanew@yandex.ru>
2020-03-19 23:23:17 +0300

    Написал запрос к серверу по первой демонстрации

---
+++ js/backend.js
@@ -0,0 +1,50 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/kekstagram/data';
+  var xhr = new XMLHttpRequest();
+
+  xhr.responseType = 'json';
+  xhr.timeout = 10000;
+
+  xhr.addEventListener('load', function () {
+    var error;
+    switch (xhr.status) {
+      case 200:
+        // successHandler(xhr.response);
+        // console.log(xhr.response);
+        break;
+
+      case 400:
+        error = 'Неверный запрос';
+        break;
+
+      case 401:
+        error = 'Пользователь не авторизован';
+        break;
+
+      case 404:
+        error = 'Ничего не найдено';
+        break;
+
+      default:
+        error = 'Статус ответа: ' + xhr.status + window.util.SPACE + xhr.statusText;
+    }
+
+    if (error) {
+      // errorHandler(error);
+    }
+  });
+
+  // xhr.addEventListener('error', function () {
+  //   errorHandler('Ошибка соединения');
+  // });
+
+  // xhr.addEventListener('timeout', function () {
+  //   var timeoutInSeconds = Math.floor(xhr.timeout / 1000);
+  //   errorHandler('Запрос выполняется слишком долго (дольше ' + timeoutInSeconds + ' секунд)');
+  // });
+
+  xhr.open('GET', URL);
+  xhr.send();
+})();
