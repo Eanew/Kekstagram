8f5e34f64f2c25c2e53626fba6cbdfc4a05520f7
Eanew <H.Eanew@yandex.ru>
2020-03-22 00:17:22 +0300

    Сообщения о статусе отправки формы

--- js/upload.js
+++ js/upload.js
@@ -4,74 +4,115 @@
   var EMPTY_SPACE_IN_EDGES_MATCH = /^\s+|\s+(?!.)/g;
   var URL = 'https://js.dump.academy/kekstagram';
 
-  var uploadForm = document.querySelector('.img-upload__form');
-  var hashTagInput = uploadForm.querySelector('.text__hashtags');
-  var uploadButton = uploadForm.querySelector('#upload-submit');
   var pageMain = document.querySelector('main');
+  var uploadForm = document.querySelector('.img-upload__form');
+  var uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
+  var hashTagInput = uploadForm.querySelector('.text__hashtags');
   var successMessage = document.querySelector('#success').content.querySelector('.success');
   var errorMessage = document.querySelector('#error').content.querySelector('.error');
+  var uploadingMessageTemplate = document.querySelector('#messages').content.querySelector('.img-upload__message');
+  var uploadingMessage;
 
-  var showResultMessage = function (template) {
+  var showUploadingMessage = function (xhr) {
+    uploadingMessage = uploadingMessageTemplate.cloneNode(true);
+    uploadingMessage.addEventListener('keydown', function (evt) {
+      if (evt.key === window.util.ESC_KEY) {
+        evt.stopPropagation();
+        xhr.abort();
+        uploadingMessage.remove();
+        uploadOverlay.classList.remove('hidden');
+      }
+    });
+    uploadOverlay.classList.add('hidden');
+    pageMain.insertAdjacentElement('afterbegin', uploadingMessage);
+    uploadingMessage.focus();
+  };
+
+  var showResultMessage = function (template, errorDescription) {
     var overlay = template.cloneNode(true);
-    var message = overlay.querySelector('div');
+    var inner = overlay.querySelector('div');
     var button = overlay.querySelector('button');
+    var message = overlay.querySelector('h2');
+
+    if (errorDescription) {
+      message.innerHTML += errorDescription;
+    }
+
+    var closeResultMessage = function () {
+      document.removeEventListener('keydown', overlayEscPressHandler);
+      overlay.remove();
+      window.util.setModalClosedMode();
+    };
 
     var overlayEscPressHandler = function (evt) {
       if (evt.key === window.util.ESC_KEY) {
-        document.removeEventListener('keydown', overlayEscPressHandler);
-        overlay.remove();
+        closeResultMessage();
       }
     };
     document.addEventListener('keydown', overlayEscPressHandler);
 
-    message.addEventListener('click', function (evt) {
+    inner.addEventListener('click', function (evt) {
       evt.stopPropagation();
     });
 
     overlay.addEventListener('click', function () {
-      document.removeEventListener('keydown', overlayEscPressHandler);
-      overlay.remove();
+      closeResultMessage();
     });
 
     button.addEventListener('click', function () {
-      document.removeEventListener('keydown', overlayEscPressHandler);
-      overlay.remove();
+      closeResultMessage();
     });
 
+    window.uploadOverlay.close();
+    window.util.setModalOpenedMode();
+    overlay.style.zIndex = '1000';
+    uploadingMessage.remove();
     pageMain.insertAdjacentElement('afterbegin', overlay);
     button.focus();
   };
 
-  var getSubmitResult = function (messageTemplate) {
-    showResultMessage(messageTemplate);
-    window.uploadOverlay.close();
-    uploadButton.textContent = 'Опубликовать';
-    uploadButton.disabled = false;
-  };
-
   var sendFormData = function (data) {
+    var error;
     var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = 5000;
+    xhr.timeout = 10000;
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        getSubmitResult(successMessage);
-      } else {
-        getSubmitResult(errorMessage);
+      switch (xhr.status) {
+        case 200:
+          error = '';
+          showResultMessage(successMessage);
+          break;
+        case 400:
+          error = ':<br><br><small>Неверный запрос</small>';
+          break;
+        case 401:
+          error = ':<br><br><small>Пользователь не авторизован</small>';
+          break;
+        case 404:
+          error = ':<br><br><small>По вашему запросу ничего не найдено</small>';
+          break;
+        default:
+          error = '.<br><br><small>Статус ответа: ' + xhr.status + window.util.SPACE + xhr.statusText + '</small>';
+      }
+      if (error) {
+        showResultMessage(errorMessage, error);
       }
     });
 
     xhr.addEventListener('error', function () {
-      getSubmitResult(errorMessage);
+      error = ':<br><br><small>Нет подключения к интернету</small>';
+      showResultMessage(errorMessage, error);
     });
 
     xhr.addEventListener('timeout', function () {
-      getSubmitResult(errorMessage);
+      var timeoutInSeconds = Math.floor(xhr.timeout / 1000);
+      error = ':<br><br><small>Превышено время выполнения (' + timeoutInSeconds + ' секунд)</small>';
+      showResultMessage(errorMessage, error);
     });
 
     xhr.open('POST', URL);
     xhr.send(data);
+    showUploadingMessage(xhr);
   };
 
   uploadForm.addEventListener('submit', function (evt) {
@@ -80,7 +121,5 @@
     .replace(EMPTY_SPACE_IN_EDGES_MATCH, '')
     .replace(window.util.EMPTY_SPACE_MATCH, window.util.SPACE);
     sendFormData(new FormData(uploadForm));
-    uploadButton.textContent = 'Отправляем...';
-    uploadButton.disabled = true;
   });
 })();
